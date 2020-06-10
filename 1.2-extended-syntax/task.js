53313a1853e53faf3173ea15433f443ed216458b
Ksenia Sidorova <nekfi@mail.ru>
2020-06-10 13:27:24 +0300

    Внесены правки

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,8 @@
-
+"use strict";
 function getResult(a,b,c) {
-    "use strict"
     let discriminant = (b ** 2) - (4 * a * c);
     let x = [];
-    if (discriminant < 0) {
-        x = [];
-    } else if (discriminant === 0) {
+    if (discriminant === 0) {
         x = [-b / (2 * a)];
     } else if (discriminant > 0) {
         x.push((-b + (Math.sqrt(discriminant))) / (2 * a));
@@ -31,19 +28,19 @@ function getAverageMark(marks) {
     let averageMark = 0;
     averageMark = sum / marks.length;
  
-
-    
     return averageMark;
     
 }
 
 function askDrink(name, dateOfBirthday){
+    console.log(dateOfBirthday);
     let result = (new Date().getFullYear() - dateOfBirthday);
+    let age = result;
 
-    if (result > 18) {
-        alert("Не желаете ли олд-фэшн, " + name + " ?");
-    } else if (result < 18) {
-        alert("Сожалею, " + name + ", но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!");
+    if (age > 18) {
+        return "Не желаете ли олд-фэшн, " + name + "?";
+    } else if (age < 18) {
+        return "Сожалею, " + name + ", но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!";
     }
-    return result;
+    return age;
 }
