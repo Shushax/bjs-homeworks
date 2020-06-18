7ddd062e2fc820fcee2fc80e8b6365881787377f
Ksenia Sidorova <nekfi@mail.ru>
2020-06-18 13:21:53 +0300

    Сделано ДЗ

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,35 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function(string) {
+    let reverseString = string.split('').reverse().join('');
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (reverseString === string) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+function getAverageMark(marks) {
+    let mark;
+    let sum = 0;
+    for (mark of marks) {
+        sum += mark;
+    }
+
+    let average = sum / marks.length;
+
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+}
+
+let now = Date.now();
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    birthday = new Date(birthday).getTime();
+    let diff = now - birthday;
+    let msInYear = 31536000000;
+    let age = diff / msInYear;
+    if (age > 18) {
+        result = "Да";
+    } else {
+        result = "Нет";
+    }
 }
