5bad3cc42fecf2cc724b4c478dd1eb212ee2e090
Ksenia Sidorova <nekfi@mail.ru>
2020-07-01 00:16:51 +0300

    добавлена функция testCase

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -60,15 +60,17 @@ class AlarmClock {
     }
 }
 
-let phoneAlarm = new AlarmClock;
-phoneAlarm.addClock("09:00", () => console.log("Пора вставать"), 1);
-phoneAlarm.addClock("09:01", () => { console.log("Давай, вставай уже!"); phoneAlarm.removeClock(2) }, 2);
-phoneAlarm.addClock("09:01", () => console.log("Иди умываться"));
-phoneAlarm.addClock("09:02", () => {
-    console.log("Вставай, а то проспишь!");
-    phoneAlarm.clearAlarms();
+function testCase() {
+    let phoneAlarm = new AlarmClock;
+    phoneAlarm.addClock("09:00", () => console.log("Пора вставать"), 1);
+    phoneAlarm.addClock("09:01", () => { console.log("Давай, вставай уже!"); phoneAlarm.removeClock(2) }, 2);
+    phoneAlarm.addClock("09:01", () => console.log("Иди умываться"));
+    phoneAlarm.addClock("09:02", () => {
+        console.log("Вставай, а то проспишь!");
+        phoneAlarm.clearAlarms();
+        phoneAlarm.printAlarms();
+    }, 3);
+    phoneAlarm.addClock("09:05", () => console.log("Вставай, а то проспишь!"), 1);
     phoneAlarm.printAlarms();
-}, 3);
-phoneAlarm.addClock("09:05", () => console.log("Вставай, а то проспишь!"), 1);
-phoneAlarm.printAlarms();
-phoneAlarm.start();
+    phoneAlarm.start(); 
+}
